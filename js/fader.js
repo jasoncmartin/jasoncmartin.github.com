0be80f92adbea74c78a9c7f21fa953d9fd09da19
jasoncmartin <legos.jedi@gmail.com>
2010-02-23 20:49:04 -0500

    Added js folder.

---
+++ js/fader.js
@@ -0,0 +1,33 @@
+if(typeof window.console == "undefined") {
+	window.console = {
+		log: function() { }
+	};
+}
+
+function rotator() {
+	var active = $$(".active")[0];
+	
+	var next = active.getNext(".app");
+	
+	console.log([active, next]);
+	
+	if(next == null) {
+		next = active.getParent().getFirst(".app");
+		
+		new Fx.Tween(next, {'onComplete': function() {
+			next.addClass("active");
+			active.removeClass("active");
+		}}).start('opacity', 0, 1);
+		
+		return;
+	}
+	
+	new Fx.Tween(active, {'onComplete': function() {
+		next.addClass("active");
+		active.removeClass("active");
+	}}).start('opacity', 1, 0);
+}
+
+window.addEvent("load", function() {
+	rotator.periodical(5000);
+});
